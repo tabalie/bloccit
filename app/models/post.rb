5cbbae8ac44e2c9461b91e9a8fa05f233a91c014
Natalie Kueneman <tabalie@gmail.com>
2015-06-03 22:46:55 -0600

    Assignment 38 completed, scopes

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,10 @@ class Post < ActiveRecord::Base
   has_many :comments
   belongs_to :user
 
+  posts = Post.all
+
   default_scope { order('created_at DESC') }
+  scope :ordered_by_title, -> { posts.order(:title) }
+  scope :ordered_by_reverse_created_at, -> { posts.order('created_at DESC').reverse }
+
 end
