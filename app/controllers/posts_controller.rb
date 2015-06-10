0ade3d98cee9dc4c43e0c2b5c25c0d46bc62b315
Natalie Kueneman <tabalie@gmail.com>
2015-06-10 15:39:19 -0600

    Assignment 37 completed, added FLASH ATTACK.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,7 @@
 class PostsController < ApplicationController
  
-  # before_action :flash_attack
-
-  # private 
-    
-  # def flash_attack
-  #   flash[:warning] = "FLASH ATTACK"
-  # end
-
+  skip_before_action :flash_attack, only: [:index, :new]
+  
   def index
       @posts = Post.all
   end
