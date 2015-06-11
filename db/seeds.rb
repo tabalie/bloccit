239fc8ecba4c32b39540ebafcd4047403c721124
Natalie Kueneman <tabalie@gmail.com>
2015-06-10 21:51:36 -0600

    Checkpoint 40 completed, Topics and Posts.

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,15 +2,15 @@ require 'faker'
 
 # Create Users
  5.times do
-   user = User.new(
-     name:     Faker::Name.name,
-     email:    Faker::Internet.email,
-     password: Faker::Lorem.characters(10)
-   )
-   user.skip_confirmation!
-   user.save!
- end
- users = User.all
+    user = User.new(
+      name:     Faker::Name.name,
+      email:    Faker::Internet.email,
+      password: Faker::Lorem.characters(10)
+    )
+    user.skip_confirmation!
+    user.save!
+  end
+  users = User.all
  
  # Note: by calling `User.new` instead of `create`,
  # we create an instance of User which isn't immediately saved to the database.
@@ -20,32 +20,66 @@ require 'faker'
  
  # The `save` method then saves this User to the database.
  
+ # Create Topics
+ 15.times do
+    Topic.create!(
+      name: Faker::Lorem.sentence,
+      description: Faker::Lorem.paragraph
+    )
+  end
+  topics = Topic.all
+
 
  # Create Posts
  50.times do
-   Post.create!(
-    user: users.sample,
-     title:  Faker::Lorem.sentence,
-     body:   Faker::Lorem.paragraph
-   )
- end
+    Post.create!(
+      user: users.sample,
+      topic: topics.sample,
+      title:  Faker::Lorem.sentence,
+      body:   Faker::Lorem.paragraph
+    )
+  end
  posts = Post.all
  
  # Create Comments
  100.times do
-   Comment.create!(
+    Comment.create!(
     # user: users.sample,   # we have not yet associated Users with Comments
-     post: posts.sample,
-     body: Faker::Lorem.paragraph
-   )
- end
+      post: posts.sample,
+      body: Faker::Lorem.paragraph
+    )
+  end
 
- user = User.first
- user.skip_reconfirmation!
- user.update_attributes!(
-   email: 'tabalie@gmail.com',
+# Create an admin user
+ admin = User.new(
+   name:     'Admin User',
+   email:    'admin@example.com',
+   password: 'helloworld',
+   role:     'admin'
+ )
+ admin.skip_confirmation!
+ admin.save!
+ 
+ # Create a moderator
+ moderator = User.new(
+   name:     'Moderator User',
+   email:    'moderator@example.com',
+   password: 'helloworld',
+   role:     'moderator'
+ )
+ moderator.skip_confirmation!
+ moderator.save!
+ 
+ # Create a member
+ member = User.new(
+   name:     'Member User',
+   email:    'member@example.com',
    password: 'helloworld'
  )
+ member.skip_confirmation!
+ member.save!
+
+
  
  puts "Seed finished"
  puts "#{User.count} users created"
