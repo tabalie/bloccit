e5623da492680a76c4150235b6d8e122bb18928e
Natalie Kueneman <tabalie@gmail.com>
2015-06-15 16:58:27 -0600

    Checkpoint 41 complete, Interlude - config Bloccit for Production environment.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,48 +1,53 @@
 class PostsController < ApplicationController
 
   def show
-      @topic = Topic.find(params[:topic_id])
-      @post = Post.find(params[:id])
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
   end
 
   def new
-      @topic = Topic.find(params[:topic_id])
-      @post = Post.new
-      authorize @post
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.new
+    authorize @post
   end
 
   def create
-      @topic = Topic.find(params[:topic_id])
-      @post = current_user.posts.build(params.require(:post).permit(:title, :body))
-      @post.topic = @topic
-      authorize @post
-      if @post.save
-        flash[:notice] = "Post was saved."
-        redirect_to [@topic, @post]
-
-      else
-        flash[:error] = "There was an error saving the post. Please try again."
-        render :new
-      end
+    @topic = Topic.find(params[:topic_id])
+    @post = current_user.posts.build(post_params)
+    @post.topic = @topic
+    authorize @post
+    if @post.save
+      flash[:notice] = "Post was saved."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :new
+    end
   end
 
   def edit
-      @topic = Topic.find(params[:topic_id])
-      @post = Post.find(params[:id])
-      authorize @post
+    @topic = Topic.find(params[:topic_id])
+    @post = Post.find(params[:id])
+    authorize @post
   end
 
   def update
-      @topic = Topic.find(params[:topic_id])
-      @post = Post.find(params[:id])
-      authorize @post
-      if @post.update_attributes(params.require(:post).permit(:title, :body))
-        flash[:notice] = "Post was updated."
-        redirect_to [@topic, @post]
-      else
-        flash[:error] = "There was an error saving the post. Please try again."
-        render :edit
-      end
+    @topic = Topic.find(params[:topic_id])
+    @post = current_user.posts.build(post_params)
+    authorize @post
+    if @post.update_attributes(params.require(:post).permit(:title, :body))
+      flash[:notice] = "Post was updated."
+      redirect_to [@topic, @post]
+    else
+      flash[:error] = "There was an error saving the post. Please try again."
+      render :edit
+    end
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:title, :body)
   end
 
 end
