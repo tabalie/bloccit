5bf68bfcb09406cdfa7142f2e186f57b299607dd
Natalie Kueneman <tabalie@gmail.com>
2015-06-30 11:33:13 -0600

    Comment delete function working properly.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
     @topic = Topic.find(params[:topic_id])
     @post = Post.find(params[:id])
     @comments = @post.comments
-    @comment = @post.comments.build
+    @comment = Comment.new
   end
 
   def new
