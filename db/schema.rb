c7185795b89aefb709360f766ba39a7b94ccca67
Natalie Kueneman <tabalie@gmail.com>
2015-07-22 09:37:28 -0600

    Progress on CP 49, voting. Vote model created and migration worked successfully.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150722020347) do
+ActiveRecord::Schema.define(version: 20150722023038) do
 
   create_table "comments", force: :cascade do |t|
     t.text     "body"
@@ -71,13 +71,6 @@ ActiveRecord::Schema.define(version: 20150722020347) do
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "votes", force: :cascade do |t|
-    t.integer  "user_id"
-    t.integer  "post_id"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
-
-  add_index "votes", ["post_id"], name: "index_votes_on_post_id"
-  add_index "votes", ["user_id"], name: "index_votes_on_user_id"
 
 end
