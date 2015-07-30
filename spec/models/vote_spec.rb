a6f762d651ba3877a1ba6864eca66baec33e6e8d
Natalie Kueneman <tabalie@gmail.com>
2015-07-29 22:13:13 -0600

    Futher progress on CP 49

--- spec/models/vote_spec.rb
+++ spec/models/vote_spec.rb
@@ -1,22 +1,18 @@
-require 'rails_helper'
-
-validates :value, inclusion: { in: [-1, 1], message: "%{value} is not a valid vote." }
-
 describe Vote do
   describe "validations" do
     describe "value validation" do
       it "only allows -1 or 1 as values" do
         v1 = Vote.new(value: 1)
-        expect (v1.valid?).to_eq(true)
+        expect(v1.valid?).to_eq(true)
      
         v2 = Vote.new(value: -1)
-        expect (v2.valid?).to_eq(true)
+        expect(v2.valid?).to_eq(true)
 
         v3 = Vote.new(value: 2)
-        expect (v3.valid).to_eq(false)
+        expect(v3.valid).to_eq(false)
 
         v4 = Vote.new(value: -2)
-        expect (v4.valid).to_eq(false)
+        expect(v4.valid).to_eq(false)
       end  
     end
   end
