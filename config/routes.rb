0fa48a2ea818cfc76b9afcb4930edda0a1b767fb
Natalie Kueneman <tabalie@gmail.com>
2015-07-30 22:33:23 -0600

    Progress on CP 49, Voting, up to authorizing votes.

--- config/routes.rb
+++ config/routes.rb
@@ -9,13 +9,10 @@ Rails.application.routes.draw do
 
   resources :posts, only: [] do
     resources :comments, only: [:create, :destroy]
+
+    post '/up-vote' => 'votes#up_vote', as: :up_vote
+    post '/down-vote' => 'votes#down_vote', as: :down_vote
   end
-  
-  # resources :topics do
-  #   resources :posts, except: [:index] do
-  #     resources :comments, only: [:create, :destroy]
-  #   end
-  # end
   
   get 'about' => 'welcome#about'
 
