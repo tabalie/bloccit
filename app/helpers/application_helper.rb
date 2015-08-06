43bffdd1a600a47c554b523b1ce229dda1eb7ef3
Natalie Kueneman <tabalie@gmail.com>
2015-08-05 23:41:29 -0600

    Checkpoint AND assignment completed, Another Interlude.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,4 +13,19 @@ module ApplicationHelper
     redcarpet = Redcarpet::Markdown.new(renderer, extensions)
     (redcarpet.render markdown).html_safe
   end
+
+  def down_vote_link_classes(post)
+    base = "glyphicon glyphicon-chevron-down "
+    vote = current_user.voted(post)
+    base += "voted" if vote && vote.down_vote?
+    base
+  end
+
+  def up_vote_link_classes(post)
+    base = "glyphicon glyphicon-chevron-up "
+    vote = current_user.voted(post)
+    base += "voted" if vote && vote.up_vote?
+    base
+  end
+
 end
