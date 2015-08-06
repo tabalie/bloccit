922b8123f95b482e94cf309e50705f4cf8eb6959
Natalie Kueneman <tabalie@gmail.com>
2015-08-05 21:52:18 -0600

    Progress on Assignment 52, private topics.

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,6 +1,8 @@
 class Topic < ActiveRecord::Base
   has_many :posts, dependent: :destroy
 
-  scope :visible_to, -> (user) { user ? all : where(public: true) }
+  scope :visible_to, -> (user) :publicly_viewable
+  scope :publicly_viewable, -> (user) { user ? all : where(public: true) }
+  scope :privately_viewable, -> (user) { user ? all : where(public: false) }
 
 end
