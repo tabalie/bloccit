1ceb606308d445138a249413f2062e9d529c5dff
Natalie Kueneman <tabalie@gmail.com>
2015-08-05 23:30:47 -0600

    Progress on CP53, formatting up and down errors still not working.

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -12,26 +12,45 @@ describe Comment do
       @other_user = authenticated_user
       @comment = Comment.new(body: 'My comment is really great', post: @post, user: @other_user)
     end
- 
-    it "sends an email to users who have favorited the post" do
-      favorite = @user.favorites.create(post: @post)
- 
-      allow( FavoriteMailer )
-        .to receive(:new_comment)
-        .with(@user, @post, @comment)
-        .and_return( double(deliver_now: true) )
 
-        expect( FavoriteMailer )
+    # We don't need to change anything for this condition;
+    # The email_favorites attribute defaults to true
+    context "with user's permission" do
+   
+      it "sends an email to users who have favorited the post" do
+        favorite = @user.favorites.create(post: @post)
+   
+        allow( FavoriteMailer )
           .to receive(:new_comment)
+          .with(@user, @post, @comment)
+          .and_return( double(deliver_now: true) )
+
+          expect( FavoriteMailer )
+            .to receive(:new_comment)
+   
+        @comment.save
+      end
  
-      @comment.save
+      it "does not send emails to users who haven't" do
+        expect( FavoriteMailer )
+          .not_to receive(:new_comment)
+   
+        @comment.save
+      end
     end
+
+    context "without permission" do
  
-    it "does not send emails to users who haven't" do
-      expect( FavoriteMailer )
-        .not_to receive(:new_comment)
+      before { @user.update_attribute(:email_favorites, false) }
  
-      @comment.save
+      it "does not send emails, even to users who have favorited" do
+        @user.favorites.where(post: @post).create
+ 
+        expect( FavoriteMailer )
+          .not_to receive(:new_comment)
+ 
+        @comment.save
+      end
     end
   end
 end
