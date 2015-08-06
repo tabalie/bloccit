48ff9eed5db937eb8121d08342db64648c6bacee
Natalie Kueneman <tabalie@gmail.com>
2015-08-05 21:27:11 -0600

    Checkpoint 52 completed, private topics.

--- app/policies/topic_policy.rb
+++ app/policies/topic_policy.rb
@@ -1,15 +1,19 @@
- class TopicPolicy < ApplicationPolicy
+class TopicPolicy < ApplicationPolicy
  
-   def index?
-     true
-   end
+  def index?
+    true
+  end
  
-   def create?
-     user.present? && user.admin?
-   end
+  def create?
+    user.present? && user.admin?
+  end
  
-   def update?
-     create?
-   end
+  def update?
+    create?
+  end
+
+  def show?
+    record.public? || user.present?
+  end
  
- end
+end
